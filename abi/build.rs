48147e4bde996c7e58d2446f3a7d55d484fe8811
etscript <etscript@163.com>
2022-12-19 14:56:32 +0800

    add abi

---
+++ abi/build.rs
@@ -0,0 +1,17 @@
+// use std::fs;
+use std::process::Command;
+
+fn main() {
+    // fs::create_dir_all("src/pb").unwrap();
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .out_dir("src/pb")
+        .compile(&["protos/reservation.proto"], &["protos"])
+        .unwrap();
+
+    // fs::remove_file("src/pb/google.protobuf.rs").unwrap();
+
+    Command::new("cargo").args(&["fmt"]).output().unwrap();
+
+    println!("cargo:rerun-if-changed=protos/reservation.proto");
+}
