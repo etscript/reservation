cc65dacd47e325b9d089de87574ccf50c49452f5
etscript <etscript@163.com>
2022-12-20 23:49:34 +0800

    add test

--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -1,4 +1,6 @@
 mod pb;
+use std::fmt;
+
 pub use pb::*;
 
 use chrono::{DateTime, NaiveDateTime, Utc};
@@ -9,4 +11,22 @@ pub fn convert_to_utc_timestamp(ts: Timestamp) -> DateTime<Utc> {
         NaiveDateTime::from_timestamp(ts.seconds, ts.nanos as _),
         Utc,
     )
+}
+
+pub fn convert_to_timestamp(dt: DateTime<Utc>) -> Timestamp {
+    Timestamp {
+        seconds: dt.timestamp(),
+        nanos: dt.timestamp_subsec_nanos() as _,
+    }
+}
+
+impl fmt::Display for ReservationStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ReservationStatus::Pending => write!(f, "pending"),
+            ReservationStatus::Blocked => write!(f, "blocked"),
+            ReservationStatus::Confirmed => write!(f, "confirmed"),
+            ReservationStatus::Unknown => write!(f, "unknown"),
+        }
+    }
 }
--- reservation/src/manager.rs
+++ reservation/src/manager.rs
@@ -10,8 +10,8 @@ impl Rsvp for ReservationManager {
         if rsvp.start.is_none() || rsvp.end.is_none() {
             return Err(ReservationError::InvalidReservation);
         }
-        // let status = abi::ReservationStatus::from_i32(rsvp.status)
-        //     .unwrap_or(abi::ReservationStatus::Pending);
+        let status = abi::ReservationStatus::from_i32(rsvp.status)
+            .unwrap_or(abi::ReservationStatus::Pending);
 
         let start = abi::convert_to_utc_timestamp(rsvp.start.as_ref().unwrap().clone());
         let end = abi::convert_to_utc_timestamp(rsvp.end.as_ref().unwrap().clone());
@@ -19,20 +19,21 @@ impl Rsvp for ReservationManager {
         let timespan: PgRange<DateTime<Utc>> = (start..end).into();
         // generate a insert sql for the reservation
         let sql =  
-            "INSERT INTO reservation (user_id, resource_id, timespan, note, status) VALUES ($1, $2, 
-            $3, $4, $5) RETURNING id";
+            "INSERT INTO rsvp.reservations (user_id, resource_id, timespan, note, status) VALUES ($1, $2, 
+            $3, $4, $5::rsvp.reservation_status) RETURNING id";
         // execute the sql
         let id = sqlx::query(sql)
             .bind(&rsvp.user_id.clone())
             .bind(&rsvp.resource_id.clone())
             .bind(timespan)
             .bind(&rsvp.note.clone())
-            .bind(&rsvp.status)
+            .bind(status.to_string())
             .fetch_one(&self.pool)
             .await?.get(0);
         rsvp.id = id;
         Ok(rsvp)
     }
+
     // change reservation status (if current status is pending, change it to confirmed)
     async fn change_status(
         &self,
@@ -62,5 +63,38 @@ impl Rsvp for ReservationManager {
         _query: abi::ReservationQuery,
     ) -> Result<Vec<abi::Reservation>, ReservationError> {
         todo!()
+    }
+}
+
+impl ReservationManager {
+    pub async fn new(pool: sqlx::PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use abi::convert_to_timestamp;
+
+    use super::*;
+
+    #[sqlx_database_tester::test(
+        pool(variable = "migrated_pool", migrations = "../migrations"),
+    )]
+    async fn reserve_should_work_for_valid_windows(){
+        let manager = ReservationManager{pool: migrated_pool.clone()};
+        let start = convert_to_timestamp(Utc::now());
+        let end = convert_to_timestamp(Utc::now());
+        let rsvp = abi::Reservation{
+            id: 0,
+            user_id: "user1".to_string(),
+            resource_id: "resource1".to_string(),
+            start: Some(start),
+            end: Some(end),
+            note: "just note".to_string(),
+            status: abi::ReservationStatus::Pending as i32,
+        };
+        let rsvp = manager.reserve(rsvp).await.unwrap();
+        assert!(rsvp.id > 0);
     }
 }
